e779b2141026434ff00b346d01d2341cc5faba4d
nxan <nxan.developer@gmail.com>
2020-11-14 10:44:39 +0700

    yeah yeah
    yeah yeah

---
+++ app.js
@@ -0,0 +1,61 @@
+const express = require('express')
+const socketio = require('socket.io')
+const app = express()
+const path = require('path')
+var cors = require('cors')
+
+app.use(cors())
+app.use(express.static('public'));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'test.html'))
+})
+app.get('/test2', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'test2.html'))
+})
+app.get('/broadcast', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'broadcast.html'))
+})
+app.get('/4', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'test2.html'))
+})
+const server = app.listen(3000, () => {
+  console.log("Reader server")
+})
+const io = socketio(server, {
+  serveClient: true,
+  cors: {
+    origin: "https://51d94632b1f7.ngrok.io",
+    credentials: true
+  }
+})
+
+var rooms = new Map()
+
+io.on('connection', socket => {
+  console.log("New user connected")
+  socket.on("broadcaster", (args) => {
+    rooms[args.room_id] = socket.id
+    console.log("Total class: " + rooms[args.room_id])
+    // socket.broadcast.emit("broadcaster", socket.id);
+  });
+  socket.on("watcher", (args) => {
+    console.log("Total class 1: " + args.roomid)
+    console.log("Total class 1: " + rooms[args.room_id])
+
+    socket.to(rooms[args.room_id]).emit("watcher", socket.id);
+  });
+  // socket.on("disconnect", () => {
+  //   socket.to(classes[0].socket_id).emit("disconnectPeer", socket.id);
+  // });
+
+
+  socket.on("offer", (id, message) => {
+    socket.to(id).emit("offer", socket.id, message);
+  });
+  socket.on("answer", (id, message) => {
+    socket.to(id).emit("answer", socket.id, message);
+  });
+  socket.on("candidate", (id, message) => {
+    socket.to(id).emit("candidate", socket.id, message);
+  });
+})
---
+++ public/watch.js
@@ -0,0 +1,68 @@
+let peerConnection;
+const config = {
+  iceServers: [
+      { 
+        "urls": "stun:stun.l.google.com:19302",
+      },
+  ]
+};
+
+const socket = io.connect("https://51d94632b1f7.ngrok.io");
+const video = document.querySelector("video");
+const enableAudioButton = document.querySelector("#enable-audio");
+
+enableAudioButton.addEventListener("click", enableAudio)
+
+socket.on("offer", (id, description) => {
+  peerConnection = new RTCPeerConnection(config);
+  peerConnection
+    .setRemoteDescription(description)
+    .then(() => peerConnection.createAnswer())
+    .then(sdp => peerConnection.setLocalDescription(sdp))
+    .then(() => {
+      socket.emit("answer", id, peerConnection.localDescription);
+    });
+  peerConnection.ontrack = event => {
+    video.srcObject = event.streams[0];
+  };
+  peerConnection.onicecandidate = event => {
+    if (event.candidate) {
+      socket.emit("candidate", id, event.candidate);
+    }
+  };
+});
+
+
+socket.on("candidate", (id, candidate) => {
+  peerConnection
+    .addIceCandidate(new RTCIceCandidate(candidate))
+    .catch(e => console.error(e));
+});
+
+socket.on("connect", () => {
+  // socket.emit("watcher");
+  console.log("Connected")
+});
+
+socket.on("broadcaster", (socketid) => {
+  // socket.emit("watcher", socketid);
+});
+
+socket.on("disconnectPeer", () => {
+  peerConnection.close();
+});
+
+window.onunload = window.onbeforeunload = () => {
+  socket.close();
+};
+
+function enableAudio() {
+  console.log("Enabling audio")
+  video.muted = false;
+}
+
+let watch = document.querySelector('#watch')
+watch.addEventListener('click', e => {
+  let roomid = document.querySelector('#roomid')
+  socket.emit("watcher", {"roomid": roomid});
+})
